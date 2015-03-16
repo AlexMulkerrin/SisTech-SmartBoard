6c6e41f7dd66b467da604c5fdf95b36200cf5ff3
Hilary Hastings <h.hastings.14@aberdeen.ac.uk>
2015-03-16 13:04:41 +0000

    changed html to php

--- Source/webPortal/index.php
+++ Source/webPortal/index.php
@@ -1,92 +1,28 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
 <html xmlns="http://www.w3.org/1999/xhtml">
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-<head>
-	<title>sistech als</title>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" type="text/css" href="style.css"/>
-	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+   <head>
+  
+	 
 </head>
 <body>
-<div class="container">
- <div class="innercontainer">
-  <div class="contentcontainer">
-	<div class="blurbcontainerleft">
-	<div class="blurbheader"> Reminders </div>
-	<?php
-		$servername = "mysql.abdn.ac.uk";
-		$username = "t02hah14_sistech";
-		$password = "sistech";
-		$dbname = "t02hah14_sistech";
-		$userid=1;
-		$task_no=1;
-		// Create connection and read tasks
-		$conn = new mysqli($servername, $username, $password, $dbname);
+	<div id="container">       
+<form>
+    <center><label for="username">Username:</label>
+<input type="text" id="username" name="username">
+    <br></br>
+<label for="password">Password:</label>
+<input type="password" id="password" name="password">
+<div id="lower">
+<input type="checkbox"><label for="checkbox">Keep me logged in</label>
+<input type="submit" value="Login">
+</br>
+<a href="C:\Users\ATTAH\Documents\NetBeansProjects\sitechALT\public_html\signupage.html">
+    <button type="button">Add profile</button></a></center>
+</div><!--/ lower-->
 
-		// Check connection
-		if ($conn->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
-		}
-		
-		$sql = "SELECT rem_table_key, uid, reminder_date, reminder_time_by, reminder_text, reminder_task_completed FROM reminders
-		WHERE s_uid =$userid AND reminder_task_completed =0
-		ORDER BY reminder_date ASC , reminder_time_by ASC
-		LIMIT 0 , 30";
-		
-		$result = $conn->query($sql);
-		
-		if ($result->num_rows > 0) {
-		// output data of each row
-		while($row = $result->fetch_assoc()) {
-		echo $task_no.". ".$row["reminder_time_by"]. " " . $row["reminder_text"]."<br>";
-		$task_no+=1;
-		}
-		} else {
-		echo "0 results";
-		}
-		$conn->close();
-	?>
-	</div>
-
-	<div class="blurbcontainerright">
-	 <div class="blurbheader"> Messages </div>
-	<?php
-		$servername = "mysql.abdn.ac.uk";
-		$username = "t02hah14_sistech";
-		$password = "sistech";
-		$dbname = "t02hah14_sistech";
-		$userid=1;
-		$task_no=1;
-		// Create connection and read tasks
-		$conn = new mysqli($servername, $username, $password, $dbname);
-		// Check connection
-		if ($conn->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
-		}
-		$sql = "SELECT rem_table_key, uid, reminder_date, reminder_time_by, reminder_text, reminder_task_completed FROM reminders
-		WHERE s_uid =$userid AND reminder_task_completed =0
-		ORDER BY reminder_date ASC , reminder_time_by ASC
-		LIMIT 0 , 30";
-		$result = $conn->query($sql);
-		if ($result->num_rows > 0) {
-		// output data of each row
-		while($row = $result->fetch_assoc()) {
-		echo $task_no.". ".$row["reminder_time_by"]. " " . $row["reminder_text"]."<br>";
-		}
-		} else {
-			echo "0 results";
-			}
-			$conn->close();
-	?>
-	</div>
-  </div>
- </div>
+</form>
 </div>
+	
 </body>
 </html>
