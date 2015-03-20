7e24ef5ffb5a08d81a527db362bd3838b6d35206
prinoldm <prinold.m.j@outlook.com>
2015-03-20 12:32:36 +0000

    Update MessageImage.java

--- Source/Prototype/Sistech/src/sistech/MessageImage.java
+++ Source/Prototype/Sistech/src/sistech/MessageImage.java
@@ -5,30 +5,42 @@
  */
 package sistech;
 
-import java.io.*;
+
 import java.awt.*;
+import java.io.*;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
-import javax.swing.JPanel;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MessageImage 
 {
     /*
-     * Method to save the image of the new handwritten message and call the addmessage method 
+     * Method to save the image of the new handwritten message 
+     * Parameteres that need to be passed to this method when called are: 
+     * Component the handwritten message panel
+     * int the user id
+     * and call the addmessage method 
      * passing on the parameters ImagePath and MessageStream
      * return type void 
      */
-    public static void saveMessage(Component panel)
+    public static void saveMessage(Component panel, int id)
     {
         
         BufferedImage image = new BufferedImage(panel.getWidth(), panel.getHeight(), BufferedImage.TYPE_INT_RGB);
-        panel.paint(image.createGraphics());
+        Graphics2D graphics2D = image.createGraphics();
+        panel.paint(graphics2D);
+        DateFormat df = new SimpleDateFormat("yyyMMddHHmmss");
+        Date date = new Date();
+        String name = id + df.format(date);
+        
         try
         {
            
-            ImageIO.write(image, "jpg", new File("images/testing.jpg"));
+            ImageIO.write(image, "jpg", new File("images/" + name + ".jpg"));
             System.out.println("panel saved");
-            sistech.DBInformation.addMessage("images/testing.jpg", 1);
+            sistech.DBInformation.addMessage("images/" + name + ".jpg", 1);
         }
         catch(Exception e)
         {
