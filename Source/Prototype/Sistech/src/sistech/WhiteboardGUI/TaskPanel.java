d5746464476b92fa129aec59b1d0cbd0f6cd5b5f
Alex Mulkerrin <a.mulkerrin.12@aberdeen.ac.uk>
2015-03-24 09:49:45 +0000

    Update TaskPanel.java
    Cleaning up presentation of task list data from database.

--- Source/Prototype/Sistech/src/sistech/WhiteboardGUI/TaskPanel.java
+++ Source/Prototype/Sistech/src/sistech/WhiteboardGUI/TaskPanel.java
@@ -11,8 +11,8 @@ import javax.swing.*;
  * @author Alex Mulkerrin
  */
 class TaskPanel extends JPanel  implements ActionListener, Runnable {
-    public String taskContent = "Task list goes here";
-    JLabel taskContainer;
+    public String taskContent = "Task list goes here\n\n";
+    JTextArea taskContainer;
     //JButton button;
     
     public TaskPanel() {
@@ -40,7 +40,7 @@ class TaskPanel extends JPanel  implements ActionListener, Runnable {
 //            //day[i].addTask(i+1,month,year); //days start at 1 not 0   
 //            this.add(day[i]);
 //        }
-        taskContainer = new JLabel(taskContent);
+        taskContainer = new JTextArea(taskContent);
         add(taskContainer);
              
         //button = new JButton("UpdateTasks");
@@ -65,11 +65,11 @@ class TaskPanel extends JPanel  implements ActionListener, Runnable {
         while (true) {
             try {
             String[][] reminders = sistech.DBInformation.getReminder("2015-05-01");
-            String messageString ="/n";
+            String messageString ="\n";
             for (int i=0; i<reminders.length; i++) {
                 for (int j=0; j<reminders.length; j++) {
                     System.out.println(reminders[i][j].toString());
-                    taskContent= taskContent + reminders[i][j].toString() +"/n";
+                    taskContent= taskContent + reminders[i][j].toString() +"\n";
                 }
             }
             } catch (Exception ex){
@@ -78,7 +78,7 @@ class TaskPanel extends JPanel  implements ActionListener, Runnable {
 
             taskContainer.setText(taskContent);
             try {
-                Thread.sleep(1000);
+                Thread.sleep(30000);
             } catch (InterruptedException e) {
                 System.out.println("Interrupted: " + e.getMessage());
             }
@@ -87,6 +87,7 @@ class TaskPanel extends JPanel  implements ActionListener, Runnable {
     
 }
 
+//NOT USED!
 class DayPane extends JPanel {
     public DayPane() {
         //TODO set defaults?
@@ -115,13 +116,13 @@ class DayPane extends JPanel {
         }
         String[][] reminders = sistech.DBInformation.getReminder(dateString);
         
-        String messageString ="/n";
+        String messageString ="\n";
         for (int i=0; i<reminders.length; i++) {
            // System.out.println(reminders.get(i).toString());
-           // messageString= messageString + reminders.get(i).toString() +"/n";
+           // messageString= messageString + reminders.get(i).toString() +"\n";
         }
         
-        JLabel message = new JLabel(messageString);
+        JTextArea message = new JTextArea(messageString);
         
         this.add(message, BorderLayout.CENTER);
         this.add(date, BorderLayout.WEST);
